bec425f9a490f34d695501f59d2960d347b68ba2
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-11-14 16:12:53 +0530

    first

---
+++ main.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+    let data2 = {
+        'मुख्यमंत्री': ['एमएमआरडीए क्षेत्रातील घरांमध्ये गिरणी कामगारांसाठी ५० टक्के आरक्षण- मुख्यमंत्री ', 'लासलगाव बाजार समितीत कांद्याला २ हजार ६५० रुपयांचा भाव लासलगाव बाजार समितीत कांद्याला २ हजार ६५० रुपयांचा भाव लासलगाव बाजार समितीत कांद्याला २ हजार ६५० रुपयांचा भाव लासलगाव बाजार समितीत कांद्याला २ हजार ६५० रुपयांचा भाव'],
+        'घाटकोपर मुंबई': ['घाटकोपर इमारत दुर्घटनेप्रकरणी अटक करण्यात आलेले सुनील शितप यांच्या पोलिस कोठडीत ७ ऑगस्टपर्यंत वाढ ', 'मुंबई विद्यापीठाचे सर्व निकाल ५ ऑगस्टपर्यंत जाहीर होतील, तर १५  ऑगस्टपर्यंत विद्यार्थ्यांना गुणपत्रिका मिळणार - मुख्यमंत्री देवेंद्र फडणवीस'],
+        'सोने- चांदीचं ': ['सोने- चांदीचं आयात शुल्क मूल्य वाढवण्याचा केंद्रीय अबकारी आणि सीमाशुल्क मंडळाचा निर्णय', 'प्राप्तिकर विवरणपत्र सादर करण्याच्या मुदतीत ५ ऑगस्टपर्यंत वाढ']
+    };
+
+    const keys = Object.keys(data2);
+    const values = Object.values(data2)
+    var i = 0
+
+    const gElement = document.getElementById("xf2");
+    var ctm = gElement.getCTM();
+    const originalCTME = ctm.e;
+
+    ctm.e = -2000;
+
+    gElement.setAttribute("transform", `matrix(${ctm.a} ${ctm.b} ${ctm.c} ${ctm.d} ${ctm.e} ${ctm.f})`);
+    updatestring('xf2', values[i][0]);
+    gElement.style.opacity= 1;
+
+
+    gElement.style.transition = 'transform 0.5s ease';
+
+    ctm.e = originalCTME;
+    setTimeout(() => {
+        gElement.setAttribute("transform", `matrix(${ctm.a} ${ctm.b} ${ctm.c} ${ctm.d} ${ctm.e} ${ctm.f})`);
+    }, 300);
+
+    i = (i + 1) % (keys.length);
+
+    setInterval(() => {
+        ctm.e = -2000;
+        gElement.setAttribute("transform", `matrix(${ctm.a} ${ctm.b} ${ctm.c} ${ctm.d} ${ctm.e} ${ctm.f})`);
+        ctm.e = originalCTME;
+        updatestring('xf2', values[i][0]);
+        setTimeout(() => {
+            gElement.setAttribute("transform", `matrix(${ctm.a} ${ctm.b} ${ctm.c} ${ctm.d} ${ctm.e} ${ctm.f})`);
+        }, 800);
+        i = (i + 1) % (keys.length);
+    }, 5000);
+
+
+
+
+}
